8335b54149141e036c73de626846b3f7ae5c2598
Reaveskev <Reaveskev@gmail.com>
2022-07-07 15:36:28 -0700

    Final Touches

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const yes = document.querySelector(".Yes");
 const no = document.querySelector(".No");
-const memes = document.querySelector(".memes");
-const url = "https://api.imgflip.com/get_memes";
-const body = document.body;
 
 yes.addEventListener("click", () => {
   window.open((href = "index2.html"));
@@ -11,6 +8,4 @@ yes.addEventListener("click", () => {
 no.addEventListener("click", () => {
   window.close();
   window.open("Picture/Car.jpg", "Window Title", "width=500, height=450");
-
-  // window.close();
 });
